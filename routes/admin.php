61442723bcc5650742877039630531ecff22f8e1
Diego Pasten <diegopasten78@gmail.com>
2025-06-30 20:05:41 -0400

    feat: :truck: Se modifica la ruta y se vincula con dashboard de admin

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 /* C06: Ruta Admin */
-Route::get('/', function(){
-    return "hola";
-})->name('home');
+
+Route::get('/', function () {
+    return view('admin.dashboard');
+})->name('dashboard');
